59dbb2739de1f40dee4981e7fd23c328c03f4c73
EDavies2020 <emz201080@yahoo.co.uk>
2021-02-16 16:37:26 +0000

    changes to project page modal

--- assets/javascript/projects.js
+++ assets/javascript/projects.js
@@ -1,9 +1,9 @@
 function openModal() {
-  document.getElementById("myModal").style.display = "block";
+  document.getElementById("myModal1").style.display = "block";
 }
 
 function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+  document.getElementById("myModal1").style.display = "none";
 }
 
 var slideIndex = 1;
@@ -19,9 +19,46 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("mySlides1");
+  var captionText = document.getElementById("pictext1");
   var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+function openModal() {
+  document.getElementById("myModal2").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal2").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var captionText = document.getElementById("pictext2");
+  var dots = document.getElementsByClassName("demo");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
